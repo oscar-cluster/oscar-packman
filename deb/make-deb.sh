02508cf981f4a24f47ac5939724085c5e9637236
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-05-06 07:24:58 +0000

    Fix bugs and add features:   - it is possible now to create the package without to be the root user   - the package version is automatically found (from the file debian/control)   - clean scripts up
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@4702 18f9d06c-6ae2-0310-8438-938a554feff9

--- deb/make-deb.sh
+++ deb/make-deb.sh
@@ -9,53 +9,52 @@
 #   OSCAR source distribution.
 #
 
-# we get the version number (first parameter)
-number_of_args=$#
-version_number=$1
+# we get the version number from the debian/control file. This file must be updated before the creation of a new package
+version_number=`grep "Standards-Version:" debian/control | sed 's/Standards-Version: //' | sed 's/ //'`
 path=`pwd`
 
-echo "Be sure that we execute this script as root."
-echo "Be also sure before to create the package that package information is up-to-date."
+echo "Be sure before to create the package that package information is up-to-date."
 echo "Read the file deb/README for more details."
 echo "Press enter to continue or Ctrl+C to abord."
 read $toto
 
-if [ $number_of_args -eq 0 ] 
-then
-  echo "You have to specify a version number as parameter. This number should be the same than in the deb/control file." 
-else 
+echo "Creating Debian package for Packman/Depman version " ${version_number}"."
 
-  echo "Creating Debian package for Packman/Depman version " $version_number
+# we first copy everything in /tmp/deb-packman and prepare a directory for packaging
+rm -rf /tmp/deb-packman
+mkdir -p /tmp/deb-packman/packman-${version_number}
+ls -l /tmp/deb-packman/packman-${version_number}
+cp -rf ../* /tmp/deb-packman/packman-${version_number}
+cp -f Makefile /tmp/deb-packman/packman-${version_number}
+rm -rf /tmp/deb-packman/packman-${version_number}/deb
+rm -f /tmp/deb-packman/packman-${version_number}/packman-depman.spec
 
-  # we first copy everything in /tmp/deb-packman and prepare a directory for packaging
-  rm -rf /tmp/deb-packman
-  mkdir -p /tmp/deb-packman/packman-depman-$version_number
-  cp -rf ../* /tmp/deb-packman/packman-depman-$version_number
-  rm -rf /tmp/deb-packman/packman-depman-$version_number/deb
-  tar czf /tmp/deb-packman/packman-depman-$version_number.tar.gz /tmp/deb-packman/packman-depman-$version_number
+# Then we create the tarball used to create the package
+tar czf /tmp/deb-packman/packman-${version_number}.tar.gz /tmp/deb-packman/packman-${version_number}
 
-  cd  /tmp/deb-packman/packman-depman-$version_number
+cd  /tmp/deb-packman/packman-${version_number}
 
-  # we clean up the stub
-  rm -f debian/*.ex debian/*.EX
+# We clean up the stub
+rm -f debian/*.ex debian/*.EX
 
-  # we create all the files for the package
-  dh_make -c gpl --single
+# We create all the files for the package
+echo "Executing dh_make..."
+dh_make -c gpl --single  
 
-  # a complete tree of directories is now ready. We update that thanks to information we already have.
-  cd $path
-  cp Makefile /tmp/deb-packman/packman-depman-$version_number
-  cp debian/control /tmp/deb-packman/packman-depman-$version_number/debian
-  cp debian/copyright /tmp/deb-packman/packman-depman-$version_number/debian
-  
-  # we then really create the package
-  cd  /tmp/deb-packman/packman-depman-$version_number
-  dpkg-buildpackage -rfakeroot
+# A complete tree of directories is now ready. We update that thanks to information we already have.
+cd $path
+cp -f debian/control /tmp/deb-packman/packman-${version_number}/debian
+cp -f debian/copyright /tmp/deb-packman/packman-${version_number}/debian
+cp -f debian/changelog /tmp/deb-packman/packman-${version_number}/debian
 
-  # then we grab the created files :-)
-  cd .. 
-  cp -rf *.changes *.deb *.orig.tar.gz $path
+# we then really create the package
+cd  /tmp/deb-packman/packman-${version_number}
+echo "Executing dpkg-buildpackage -rfakeroot"
+ret=`/usr/bin/dpkg-buildpackage -rfakeroot`
 
-  echo "Package(s) created and available in the deb directory"
-fi
+# then we grab the created files :-)
+cd .. 
+cp -rf *.changes *.deb *.orig.tar.gz $path
+
+echo "Package(s) created and available in the deb directory"
 
