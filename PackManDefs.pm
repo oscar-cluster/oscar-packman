3403cf4f2395b8d44aa2e9898ea479eaa5b8ff93
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2008-09-26 19:27:15 +0000

    [PackMan] Fixes a bug in 3.1.0: the exportation of constants is now correct and can be safely used outside of PackMan. + prepare a 3.1.1 release.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@7473 18f9d06c-6ae2-0310-8438-938a554feff9

--- PackManDefs.pm
+++ PackManDefs.pm
@@ -1,4 +1,23 @@
+package OSCAR::PackManDefs;
+
+# $Id: ODA_Defs.pm 7426 2008-09-20 09:22:54Z valleegr $
+#
+# Copyright (c) 2008 Geoffroy Vallee <valleegr@ornl.gov>
+#                    Oak Ridge National Laboratory.
+#                    All rights reserved.
+
+use strict;
+use base qw(Exporter);
+
+
 use constant ERROR      => -1;
 use constant SUCCESS    => 0;
+
+my @ISA = qw(Exporter);
+
+our @EXPORT = qw(
+                ERROR
+                SUCCESS
+                );
 
 1;
