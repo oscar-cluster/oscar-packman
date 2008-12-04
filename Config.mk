26bf86c52ba9f29a9366b9bf72e2e73716b42494
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2008-12-04 16:56:40 +0000

    Move the libraries to a noarch directory.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/pkgsrc@7649 18f9d06c-6ae2-0310-8438-938a554feff9

--- Config.mk
+++ Config.mk
@@ -1,1 +1,5 @@
-LIBDIR ?= $(shell perl -V:vendorlib | sed s/vendorlib=\'\\/// | sed s/\'\;//)
+# -*- mode: Makefile; -*-
+
+LIBDIR	?= $(shell perl -V:vendorlib | sed s/vendorlib=\'// | sed s/\'\;//)
+# Ugly stuff the prepare LIBDIR for a usage with sed (extra "\" and so on).
+SEDLIBDIR ?= $(shell perl -V:vendorlib | sed s/vendorlib=\'// | sed s/\'\;// | sed s/\'\;// | awk '{ gsub(/\//, "\\\\\/"); print}')
